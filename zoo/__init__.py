08bc77dd681b256dc0b3ea3fe25b5ffc5a8babae
James Aules <james_aules@icloud.com>
2023-05-22 14:09:56 -0400

    models

--- zoo/__init__.py
+++ zoo/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABSE_URI'] = 'postgresql://postgres:Bratva@localhost:5432/zoo'
+    app.config['SQLALCHEMY_DATABSE_URI'] = 'postgresql://postgres:Bratva@localhost:5432/ballpy'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     from . import models
--- zoo/models.py
+++ zoo/models.py
@@ -1,3 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Reptile(db.Model):
+    __tablename__ = 'reptiles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    scientific_name = db.Column(db.String(50))
+    conservation_status = db.Column(db.String(50))
+    native_habitat = db.Column(db.String(100))
+    fun_fact = db.Column(db.Text)
+
+
+
+# The main endpoint should be /reptiles.
+# The database should be named ballpy.
+# The table should be named reptiles.
