3ca18e4e7f5f405c66c410c76e517edcee24ef7c
James Aules <james_aules@icloud.com>
2023-05-22 15:12:55 -0400

    data Db

--- zoo/rept.py
+++ zoo/rept.py
@@ -1,6 +1,15 @@
 from flask import Blueprint
-bp = Blueprint('rept', __name__, '/reptiles')
+import json
+bp = Blueprint('rept', __name__, url_prefix ='/reptiles')
+
+reptiles = json.load(open('reptiles.json'))
+print(reptiles)
 
 @bp.route('/')
 def index():
     return 'Hello world'
+
+@bp.route('/<id>')
+def show(id):
+    reptiles = reptiles[id -1]
+    return reptiles
