477ba58874bd8eff30614266da038da42ddbda22
James Aules <james_aules@icloud.com>
2023-05-22 13:08:32 -0400

    Database config

--- zoo/__init__.py
+++ zoo/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+#factory
 
 def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABSE_URI'] = 'postgresql://postgres:Bratva@localhost:5432/zoo'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     return app
+
+#database posql
+
+
